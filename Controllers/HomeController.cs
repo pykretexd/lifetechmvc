6ae06c423b2accefa405055d260066bbfc358cf0
pykrete <landysixten@gmail.com>
2022-04-28 09:14:20 +0200

    Extract from 7z

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LifeTechMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Service1()
+        {
+            return View();
+        }
+
+        public ActionResult Service2()
+        {
+            return View();
+        }
+
+        public ActionResult Service3()
+        {
+            return View();
+        }
+    }
+}
